ba59ff1e177ecef3dbb028b124a45aa7ef527128
codewithmohit <jindalmohit2018@gmail.com>
2021-08-01 18:29:50 +0530

    Progress going on

---
+++ Task2/src/train/Schedule.java
@@ -0,0 +1,21 @@
+package train;
+
+public class Schedule {
+	private int serial;
+	private String code;
+	private String station;
+	private String arr;
+	private String dep;
+	
+	public Schedule(int serial,String code,String station,String arr,String dep) {
+		this.serial = serial;
+		this.code = code;
+		this.station =station;
+		this.arr = arr;
+		this.dep = dep;
+	}
+	public void printSchedule() {
+		System.out.printf("%d %2s %3s %7s %3s\n",serial,code,station,
+				arr,dep);
+	}
+}
---
+++ Task2/src/train/TrainSchedule.java
@@ -0,0 +1,40 @@
+package train;
+
+
+
+public class TrainSchedule {
+	private Schedule schedule[];
+	
+	
+	public TrainSchedule(Schedule[] schedule) {
+		this.schedule = schedule;
+	}
+
+	public void printSchedule() {
+		System.out.println("Yasvantpur Delhi Sarai Rohilla AC Duronto Express (12213)");
+		System.out.println("--------------------------------------");
+		System.out.println("#  Code   Station        Arr.   Dep.");
+		System.out.println("--------------------------------------");
+		
+		for (Schedule x : schedule) {
+			x.printSchedule();
+		}
+		System.out.println("--------------------------------------");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Schedule schedule[] = {
+				new Schedule(1,"YPR","Yasvantpur Jn","","23.40"),
+				new Schedule(2,"GTL","Gautakal Jn","03.45","03.50"),
+				new Schedule(3,"SC","Secundrabad Jn","08.55","09.10"),
+				new Schedule(4,"BPQ","Balharshah Jn","13.30","13.35"),
+				new Schedule(5,"HBJ","Habibganj Jn","21.20","21.25"),
+				new Schedule(6,"JHS","Jhansi Jn","01.15","01.20"),
+				new Schedule(7,"DEE","Delhi S Rohilla","07.00","23.40")
+				};
+		TrainSchedule trainSchedule = new TrainSchedule(schedule);
+		trainSchedule.printSchedule();
+	}
+
+}
